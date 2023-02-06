0a1dfca16247a7ee1c45f398c1c3d74107954491
zmjack <jack@zmland.com>
2023-02-06 21:35:23 +0800

    NWin32.1.2.5

--- NWin32/PointerEvaluator.cs
+++ NWin32/PointerEvaluator.cs
@@ -13,12 +13,7 @@ namespace NWin32
         public PointerEvaluator(MemoryAccessor memoryAccessor)
         {
             Memory = memoryAccessor;
-#if NET35 || NET40 || NET45 || NET451 || NET46
-            var key = Tuple.Create("[", "]");
-#else
-            var key = ("[", "]");
-#endif
-            AddBracketFunction(key, value => Memory.Ptr(new IntPtr((long)value)).ToInt64());
+            AddBracketFunction(new("[", "]"), value => Memory.Ptr(new IntPtr((long)value)).ToInt64());
             Initialize();
         }
 
